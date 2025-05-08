709c0fa8126b7909fb0f36af689f8bb9ebe9bfd9
Jean Nibaudeau <jenibaud@2G7.42angouleme.fr>
2025-05-08 18:12:22 +0200

    remove_quotes not ready, need to fix it

--- utils.c
+++ utils.c
@@ -1,10 +1,12 @@
 char	*remove_quotes(char *str)
 {
 	int		i;
+	int		j;
 	int		quotes;
 	char	*res;
 
 	i = 0;
+	j = 0;
 	quotes = 0;
 	res = str;
 	while (str[i] != '\0')
@@ -16,6 +18,10 @@ char	*remove_quotes(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		
+		if (str[i] == 34 || str[i] == 39)
+			res[j] = str[i++];				//bug ici
+		j++;
+		i++;
 	}
+	return (res);
 }
